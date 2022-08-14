047a1c263f1f971bbdfef87c5bd69288b2d955af
Your Name <you@example.com>
2022-08-14 16:20:29 +0530

    [Megha] Add: Added UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Binary Search Tree Problems");
+            Binary<int> binarysearch = new Binary<int>(56);
+            binarysearch.Insert(30);
+            binarysearch.Insert(70);
+            binarysearch.Display();
         }
     }
 }
