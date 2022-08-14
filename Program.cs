ff0548e14f8695db018518b522d87a78e59ad243
Your Name <you@example.com>
2022-08-14 15:52:13 +0530

    [Megha] Add: Welcome To Binary Search Tree

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTree
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Binary Search Tree Problems");
+        }
+    }
+}
