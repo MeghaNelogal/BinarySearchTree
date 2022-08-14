53c520187db9427ae68715b2e02fa549db9c64be
Your Name <you@example.com>
2022-08-14 16:25:58 +0530

    [Megha] Add: Added UC2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,18 @@
             Binary<int> binarysearch = new Binary<int>(56);
             binarysearch.Insert(30);
             binarysearch.Insert(70);
+            binarysearch.Insert(22);
+            binarysearch.Insert(40);
+            binarysearch.Insert(60);
+            binarysearch.Insert(95);
+            binarysearch.Insert(11);
+            binarysearch.Insert(65);
+            binarysearch.Insert(3);
+            binarysearch.Insert(16);
+            binarysearch.Insert(63);
+            binarysearch.Insert(67);
             binarysearch.Display();
+            binarysearch.GetSize(); 
         }
     }
 }
